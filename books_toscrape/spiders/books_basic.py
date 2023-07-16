d881c991056c9cfbc8bd48dd496ad57198fe5dd8
o-yuji <py_ootsuka@yahoo.co.jp>
2023-07-16 22:57:18 +0900

    get_detail

--- books_toscrape/spiders/books_basic.py
+++ books_toscrape/spiders/books_basic.py
@@ -7,18 +7,26 @@ class BooksBasicSpider(scrapy.Spider):
     start_urls = ["https://books.toscrape.com/catalogue/category/books/fantasy_19/index.html"]
 
     def parse(self, response):
-        article = response.xpath('//article[@class="product_pod"]')
-        books = article.xpath('.//h3/a/text()').getall()
-        prices = article.xpath('.//div[@class="product_price"]/p[1]/text()').getall()
-        urls = article.xpath('.//h3/a/@href').getall()
+        books = response.xpath('//h3')
 
-        for book,price,url in zip(books,prices,urls):
-            yield{
-                'book':book,
-                'price':price,
-                'url':url
-            }
+        for book in books:
+            # yield{
+            #     'title':book.xpath('.//a/@title').get(),
+            #     'url':url,
+            #     'price':price,
+            # }
+            yield response.follow(url=book.xpath('./a/@href').get(),callback=self.parse_item)
+
 
         next_page = response.xpath('//li[@class="next"]/a/@href').get()
         if next_page:
             yield response.follow(next_page[0],callback=self.parse)
+    
+    def parse_item(self,response):
+        title = response.xpath('//div[contains(@class,"product_main")]/h1/text()').get()
+        price = response.xpath('//p[@class="price_color"]/text()').get()
+        
+        yield{
+            'title':title,
+            'price':price,
+        }
